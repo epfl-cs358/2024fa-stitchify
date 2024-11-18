e478cf01d57492fb8b1d65b9b7d144d0e3d44389
rozkoszna <42668158+rozkoszna@users.noreply.github.com>
2024-11-18 16:06:45 +0100

    Update Receiver_arduino.ino
    Changing reciever to cncShield and Nema

--- src/Receiver_arduino.ino
+++ src/Receiver_arduino.ino
@@ -1,30 +1,36 @@
-#include <Servo.h>
+#define stepPinX 2
+#define dirPinX  5
+#define enablePin 8
 
-Servo myServo;
 char mystr[10];
 int angle = 0;
+int steps = 0;
 
 void setup() {
+  pinMode(stepPinX, OUTPUT);   
+  pinMode(dirPinX, OUTPUT);   
+  pinMode(enablePin, OUTPUT); 
+
+  digitalWrite(enablePin, LOW); 
+  digitalWrite(dirPinX, HIGH);  
+
   Serial.begin(9600);
-  myServo.attach(9);
-  myServo.write(0);
 }
 
 void loop() {
-  memset(mystr, 0, sizeof(mystr));
-  Serial.readBytesUntil('\n', mystr, sizeof(mystr) - 1);
-  Serial.println(mystr);
+  memset(mystr, 0, sizeof(mystr));   
+  Serial.readBytesUntil('\n', mystr, sizeof(mystr) - 1); 
+  angle = atoi(mystr); 
 
-  angle = atoi(mystr);
-
-  if (angle >= 0 && angle <= 180) {
-    myServo.write(angle);
-    Serial.print("Servo moved to ");
-    Serial.print(angle);
-    Serial.println(" degrees");
-  } else {
-    Serial.println("Invalid angle received. Please send a value between 0 and 180.");
+  digitalWrite(dirPinX, LOW); 
+  for (int i = 0; i < steps; i++) 
+  {
+    digitalWrite(stepPinX, HIGH);
+    delayMicroseconds(500); 
+    digitalWrite(stepPinX, LOW);
+    delayMicroseconds(500);
   }
   
+  Serial.print(steps);
   delay(1000);
 }
