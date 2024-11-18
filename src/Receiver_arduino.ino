f93084705a903c47224445aa8c2c8e94b1368de8
BogdanCDev <bogdan.cirje@epfl.ch>
2024-11-18 02:19:28 +0100

    add: receiver

---
+++ src/Receiver_arduino.ino
@@ -0,0 +1,14 @@
+#include <ESP32Servo.h>
+
+Servo myServo; // Create a servo object
+
+void setup() {
+  myServo.attach(18); // Attach the servo to pin 18
+}
+
+void loop() {
+  myServo.write(0);   // Move to 0 degrees
+  delay(1000);        // Wait 1 second
+  myServo.write(180); // Move to 180 degrees
+  delay(1000);        // Wait 1 second
+}
