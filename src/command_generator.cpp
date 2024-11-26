a39ed61044ed853e78ec133bf035ac9ba0217d48
rozkoszna <42668158+rozkoszna@users.noreply.github.com>
2024-11-26 18:02:58 +0100

    Update command_generator.cpp
    Generating "number_rows" rows. Nuber of steps to be fixed.

--- src/command_generator.cpp
+++ src/command_generator.cpp
@@ -6,9 +6,17 @@ int main() {
     std::ofstream commandFile(filename);
 
     if (commandFile.is_open()) {
-        commandFile << "s1 45\n";
-        commandFile << "s2 45\n";
-        commandFile << "n 100\n";
+        int number_rows = 0;
+        std::cin>>number_rows;
+        for(int i=1; i<= number_rows; i++)
+        {
+            commandFile<<"s1 45\n";
+            commandFile<<"s2 45\n";
+            for(int step = 1; step <= 3; step ++)
+            {
+                commandFile<<"n 100\n";
+            }
+        }
         commandFile.close();
         std::cout << "File '" << filename << "' created successfully with commands." << std::endl;
     } else {
